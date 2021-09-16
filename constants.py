2b234ed75939d49cb3e487c6924a4cb9112bcb32
Arthur Viana <46693700+VianaArthur@users.noreply.github.com>
2021-09-16 00:23:32 -0300

    Update constants.py
    Fix typo

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 API_NAME = "youtube"
 API_VERSION = "v3"
 API_KEY = ""
-CREDENTIALS_FILENAME = "crendetials.json"
+CREDENTIALS_FILENAME = "credentials.json"
 AUTHORIZED_USER_FILENAME = "authorized_user.json"
 SPREADSHEET_ID = ""
